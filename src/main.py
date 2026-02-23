aa6d05e558b3b87b6df7ced038ba0dfb4a4352bb
mostech <gameboynes2@proton.me>
2026-02-23 15:22:00 +0100

    minimum viable product ahh

--- src/main.py
+++ src/main.py
@@ -1,62 +1,62 @@
-import pyautogui
-import tkinter as tk
-from pynput import keyboard
+import os
 import threading
+from pynput.keyboard import Key, Controller
+from pynput import keyboard
+import time
 
-class Gui:
+
+class Establish:
     def __init__(self):
-        self.root = tk.Tk()
+        self.break_key = ""
+        self.repeat_key = ""
+        self.interval = 1
+        self.keycon = Controller()
 
-        self.repeating_key = ""
-        self.root.geometry("300x300")
-        self.root.title("AutoKey")
-
-        self.root.repeat_key_button = tk.Button(self.root, text="Change Repeat Key",command=self.repeat_key_button_click)
-        self.root.repeat_key_button.pack()
-
-        self.root.start_repeating_button = tk.Button(self.root, text="balls", command=self.start_repeating)
-        self.root.start_repeating_button.pack()
-
-        self.root.mainloop()
-
-    def start_repeating(self):
+    def establish(self):
         while True:
-            pyautogui.write(self.repeating_key, interval=0.25)
+            self.repeat_key = input("What key would you wish to repeat?")
+            if len(self.repeat_key) == 1:
+                break
+            else:
+                print("Please enter a single character")
+        while True:
+            self.break_key = input("What key would you wish to break?")
+            if len(self.break_key) == 1:
+                break
+            else:
+                print("Please enter a single character")
+        while True:
+            self.interval = float(input("How many seconds would you like to wait between each repeat?"))
+            if self.interval >0:
+                break
+            else:
+                print("Please enter a valid interval")
 
-    def repeat_key_button_click(self):
-        repeat_root = tk.Toplevel(self.root)
-        repeat_root.geometry("300x300")
-        repeat_root.title("Repeat key Changer")
+        self.repeat()
 
-        repeat_entry = tk.Entry(repeat_root)
-        repeat_entry.pack()
-
-        def get_button():
-            self.repeating_key = repeat_entry.get()
-
-        entry_get_button = tk.Button(repeat_root, text="Change Repeat Key", command=get_button)
-        entry_get_button.pack()
-
-
-def main():
-    def on_press(key):
-        try:
-            if key.char == "8":
-                print("hthjet")
-        except AttributeError:
-            print('special key {0} pressed'.format(
-                key))
-
-    listener = keyboard.Listener(
-        on_press=on_press,
-    )
-
-    listener.start()
-    listener.join()
+    def repeat(self):
+        while True:
+            time.sleep(self.interval)
+            self.keycon.press(f"{self.repeat_key}")
 
 
-test = Gui()
-threading.Thread(target=main).start()
+def on_press(key):
+    print("\n\n",key, obj.break_key,"\n\n")
+    if str(format(key)).lower() == "'" + obj.break_key.lower() + "'":
+        os._exit(0)
 
-#pyautogui.write('Hello world!')
-#pyautogui.write('Hello world!', interval=0.25)
+listener = keyboard.Listener(
+    on_press=on_press,
+)
+
+
+obj = Establish()
+est_thread = threading.Thread(target=obj.establish)
+est_thread.start()
+
+listener.start()
+listener.join()
+
+
+
+
